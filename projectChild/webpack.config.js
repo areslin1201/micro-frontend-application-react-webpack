ab68048f576a7026bad285f7a900385c4e92207c
Ares Lin <ares.lin@quancom-tech.com>
2023-05-12 15:46:07 +0800

    webpack範例

---
+++ projectChild/webpack.config.js
@@ -0,0 +1,60 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const deps = require("./package.json").dependencies;
+require('dotenv').config({ path: './.env' });
+
+module.exports ={
+  mode: "development",
+  devServer: {
+    port: process.env.PORT || 8081
+  },
+  resolve: {
+    extensions: [".css", ".scss", ".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              url: {
+                filter: (url) => {
+                  if (url.startsWith("data:")) {
+                    return false;
+                  }
+                  return true;
+                },
+              },
+            },
+          },
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.jsx?$/,
+        use: ["babel-loader"],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+    new ModuleFederationPlugin({
+      name: "FIRST_APP",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./app": "./src/components/App",
+      },
+    }),
+  ],
+};
---
+++ projectMain/webpack.config.js
@@ -0,0 +1,55 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const deps = require("./package.json").dependencies;
+require('dotenv').config({ path: './.env' });
+
+module.exports = {
+  mode: "development",
+  devServer: {
+    port: process.env.PORT || 4001
+  },
+  resolve: {
+    extensions: [".css", ".scss", ".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              url: {
+                filter: (url) => {
+                  if (url.startsWith("data:")) {
+                    return false;
+                  }
+                  return true;
+                },
+              },
+            },
+          },
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.jsx?$/,
+        use: ["babel-loader"],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+    new ModuleFederationPlugin({
+      name: "MICRO",
+      remotes: {
+        FIRST_APP: "FIRST_APP@http://localhost:4000/remoteEntry.js",
+      },
+    }),
+  ],
+};
